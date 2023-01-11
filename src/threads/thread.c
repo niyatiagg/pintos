d6ff3fd3a04974f6fb12a169d9a50e76d2007bff
Niyati <niyatiagg4641@gmail.com>
2023-01-10 23:21:44 -0800

    adding declaration for priority compare

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -73,6 +73,7 @@ static void *alloc_frame (struct thread *, size_t size);
 static void schedule (void);
 void thread_schedule_tail (struct thread *prev);
 static tid_t allocate_tid (void);
+static bool priority_compare (const struct list_elem *a_, const struct list_elem *b_, void *aux UNUSED);
 
 /* Initializes the threading system by transforming the code
    that's currently running into a thread.  This can't work in
