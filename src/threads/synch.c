76d45ec12edcc1c73b38c42f70e8147d12b65a2d
Niyati <niyatiagg4641@gmail.com>
2023-01-11 23:26:43 -0800

    priority check added to sema

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -113,9 +113,13 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-  if (!list_empty (&sema->waiters)) 
-    thread_unblock (list_entry (list_pop_front (&sema->waiters),
-                                struct thread, elem));
+
+  if (!list_empty (&sema->waiters)) {
+      struct thread *th = list_entry (list_pop_front (&sema->waiters), struct thread, elem);
+      thread_unblock (th);
+      if (thread_current ()->priority < th->priority)
+          thread_yield ();
+  }
   sema->value++;
   intr_set_level (old_level);
 }
