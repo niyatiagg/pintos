08f15dc66d98d0634d84ead1672e8c5ec9b422ac
Niyati <niyatiagg4641@gmail.com>
2023-01-12 11:39:47 -0800

    added intr_context to sema up

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -117,8 +117,12 @@ sema_up (struct semaphore *sema)
   if (!list_empty (&sema->waiters)) {
       struct thread *th = list_entry (list_pop_front (&sema->waiters), struct thread, elem);
       thread_unblock (th);
-      if (thread_current ()->priority < th->priority)
-          thread_yield ();
+      if (thread_current ()->priority < th->priority) {
+          if(intr_context ())
+              intr_yield_on_return ();
+          else thread_yield ();
+      }
+
   }
   sema->value++;
   intr_set_level (old_level);
