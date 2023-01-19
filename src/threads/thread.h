a23faa064b1bdf4a8c3d1e2eba697dbb8d6cdbcf
Niyati <niyatiagg4641@gmail.com>
2023-01-18 21:15:29 -0800

    fixing declaration of thread_donate_priority

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -129,7 +129,7 @@ const char *thread_name (void);
 
 void thread_exit (void) NO_RETURN;
 void thread_yield (void);
-void thread_donate_priority (struct thread *, int donated_priority);
+void thread_donate_priority (struct thread *, int donated_priority, struct lock *);
 
 /* Performs some operation on thread t, given auxiliary data AUX. */
 typedef void thread_action_func (struct thread *t, void *aux);
