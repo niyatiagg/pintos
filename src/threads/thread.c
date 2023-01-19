17724e02637d781dda74f625117aa11dbe3d082d
Niyati <niyatiagg4641@gmail.com>
2023-01-19 15:02:45 -0800

    fixing compilation bugs

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -532,7 +532,7 @@ init_thread (struct thread *t, const char *name, int priority)
   t->priority = priority;
   t->old_priority = priority;
   t->magic = THREAD_MAGIC;
-  lock_init (&donated_locks);
+  list_init (&t->donated_locks);
   old_level = intr_disable ();
   list_push_back (&all_list, &t->allelem);
   intr_set_level (old_level);
