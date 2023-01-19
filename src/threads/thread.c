0f1bfaceea83ffda38df78dcfd9f0de58ccc9169
Niyati <niyatiagg4641@gmail.com>
2023-01-18 21:29:57 -0800

    fix the logic of thread_set_priority

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -381,15 +381,14 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority) 
 {
-  if(thread_current ()->donated_lock == NULL) {
-      int old_pri = thread_current ()->priority;
-      thread_current ()->priority = new_priority;
-      thread_current ()->old_priority = new_priority;
-      if (old_pri > new_priority)
-          thread_yield();
-      thread_current ()->donated_lock = NULL;
+   thread_current ()->old_priority = new_priority;
+   if (thread_current ()->donated_lock == NULL) {
+     int old_pri = thread_current ()->priority;
+     thread_current ()->priority = new_priority;
+     thread_current ()->donated_lock = NULL;
+     if (old_pri > new_priority)
+       thread_yield();
   }
-
 }
 
 /* Donate priority to a lower priority thread */
