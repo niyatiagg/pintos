1736bdbf0a3fb8c73db0db42a4b799f08c6d070f
Niyati <niyatiagg4641@gmail.com>
2023-01-26 15:14:09 +0530

    fixing bugs in syscall.h

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -21,6 +21,7 @@ static struct lock filesys_lock;
 void
 syscall_init (void) 
 {
+  lock_init(&filesys_lock);
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
@@ -44,21 +45,25 @@ syscall_handler (struct intr_frame *f)
         }
         case SYS_WAIT:
         case SYS_CREATE: {
+          bool ret;
           if(check_user_args(f->esp + 4) == NULL ||
               check_user_args(f->esp + 8) == NULL)
             thread_exit();
 
           char *file_name = (char *)(f->esp + 4);
-          unsigned initial_size = (int *)(f->esp + 8);
-          sys_create(file_name, initial_size);
+          unsigned initial_size = (unsigned *)(f->esp + 8);
+          ret = sys_create(file_name, initial_size);
+          f->eax = ret;
           break;
         }
         case SYS_REMOVE: {
+          bool ret;
           if(check_user_args(f->esp + 4) == NULL)
             thread_exit();
 
           char *file_name = (char *)(f->esp + 4);
-          sys_remove(file_name);
+          ret = sys_remove(file_name);
+          f->eax = ret;
           break;
         }
         case SYS_OPEN: {
@@ -143,11 +148,19 @@ exec (const char *file)
 bool
 sys_create (const char *file, unsigned initial_size)
 {
-  return filesys_create(file, initial_size);
+  bool ret;
+  lock_acquire (&filesys_lock);
+  ret = filesys_create(file, initial_size);
+  lock_release (&filesys_lock);
+  return ret;
 }
 
 bool
 sys_remove (const char *file_name)
 {
-  return filesys_remove(file_name);
+  bool ret;
+  lock_acquire (&filesys_lock);
+  ret = filesys_remove(file_name);
+  lock_release (&filesys_lock);
+  return ret;
 }
